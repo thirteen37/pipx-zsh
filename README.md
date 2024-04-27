e5c2f75c5840903fc654b5b4570b51476faa86d1
Yu-Xi Lim <limyuxi@gmail.com>
2024-04-27 19:07:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pipx installation and completions
+
+OMZ plugin to install [pipx](https://github.com/pypa/pipx).
---
+++ pipx-zsh.plugin.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+if ! (( $+commands[pipx] )); then
+    echo 'pipx command not found: please install via https://github.com/pypa/pipx'
+    exit
+fi
+
+# Ensure pipx binaries are on the path
+export -U PATH="$PATH:$HOME/.local/bin"
+
+# pipx completions needs argcomplete
+if ! (( $+commands[register-python-argcomplete] )); then
+    pipx install argcomplete
+fi
+
+eval "$(register-python-argcomplete pipx)"
