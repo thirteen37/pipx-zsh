36d1b29adc1b98bc2942956c5a8fb4c79ff22fc2
Yu-Xi Lim <limyuxi@gmail.com>
2024-04-27 22:00:16 +0800

    Set path anyways

--- pipx-zsh.plugin.zsh
+++ pipx-zsh.plugin.zsh
@@ -3,9 +3,6 @@
 if ! (( $+commands[pipx] )); then
     echo 'pipx command not found: please install via https://github.com/pypa/pipx'
 else
-    # Ensure pipx binaries are on the path
-    export -U PATH="$PATH:$HOME/.local/bin"
-
     # pipx completions needs argcomplete
     if ! (( $+commands[register-python-argcomplete] )); then
         pipx install argcomplete
@@ -13,3 +10,6 @@ else
 
     eval "$(register-python-argcomplete pipx)"
 fi
+
+# Ensure pipx binaries are on the path
+export -U PATH="$PATH:$HOME/.local/bin"
